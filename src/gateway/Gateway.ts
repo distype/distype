c6fa030390799ceafbb93a1b34c9b3016ab86cd3
BR88C <br88c@br88c.dev>
2022-01-08 12:06:51 -0500

    Add Gateway#updatePresence()

--- src/gateway/Gateway.ts
+++ src/gateway/Gateway.ts
@@ -339,4 +339,17 @@ export class Gateway extends TypedEmitter<GatewayEvents> {
             }
         });
     }
+
+    /**
+     * Update the bot's presence.
+     * @param presence Presence data.
+     * @param shard A shard or shards to set the presence on. A number will set the presence on a single shard with a matching ID, a number array will set the presence on all shards matching am ID in the array, and `all` will set the presence on all shards.
+     */
+    public async updatePresence (presence: DiscordTypes.GatewayPresenceUpdateData, shard: number | number[] | `all` = `all`): Promise<void> {
+        const shards = typeof shard === `number` ? [this.shards.get(shard)] : ((shard instanceof Array) ? this.shards.filter((s) => shard.some((sh) => sh === s.id)) : this.shards).map((s) => s);
+        await Promise.all(shards.map((s) => s?.send({
+            op: 3,
+            d: presence
+        })));
+    }
 }
