507833bb104cfdd581bc85dec567764c6871f9ca
BR88C <br88c@br88c.dev>
2022-04-19 10:54:51 -0400

    Fix parsing strings

--- src/gateway/GatewayShard.ts
+++ src/gateway/GatewayShard.ts
@@ -557,7 +557,7 @@ export class GatewayShard extends TypedEmitter<GatewayShardEvents> {
         try {
             if (Array.isArray(data)) data = Buffer.concat(data);
             else if (data instanceof ArrayBuffer) data = Buffer.from(data);
-            return JSON.parse(data.toString());
+            return typeof data === `string` ? data : JSON.parse(data.toString());
         } catch (error: any) {
             this._log(`Payload parsing error: ${(error?.message ?? error) ?? `Unknown reason`}`, {
                 level: `WARN`, system: this.system
