6b3b69a35d8db053119477136a0c8f4a5136cb52
BR88C <br88c@br88c.dev>
2022-04-19 14:47:12 +0000

    build: TSC build for 35b49467093edea038358856af862166b7fff67d

--- dist/gateway/GatewayShard.js
+++ dist/gateway/GatewayShard.js
@@ -177,7 +177,7 @@ class GatewayShard extends node_utils_1.TypedEmitter {
         this._killed = false;
         for (let i = 0; i < this.options.spawnMaxAttempts; i++) {
             const attempt = await this._initSocket(false).then(() => true).catch((error) => {
-                this._log(`Spawn attempt ${i}/${this.options.spawnMaxAttempts} failed: ${(error?.message ?? error) ?? `Unknown reason`}`, {
+                this._log(`Spawn attempt ${i + 1}/${this.options.spawnMaxAttempts} failed: ${(error?.message ?? error) ?? `Unknown reason`}`, {
                     level: `ERROR`, system: this.system
                 });
                 return false;
@@ -535,7 +535,8 @@ class GatewayShard extends node_utils_1.TypedEmitter {
             case DiscordTypes.GatewayOpcodes.InvalidSession: {
                 this._close(!!payload.d, payload.d ? 4000 : 1000, `Got Invalid Session (opcode ${DiscordTypes.GatewayOpcodes.InvalidSession})`);
                 this._enterState(GatewayShardState.DISCONNECTED);
-                (0, node_utils_1.wait)(2500).then(() => this._reconnect(!!payload.d));
+                if (!this._spinning)
+                    (0, node_utils_1.wait)(2500).then(() => this._reconnect(!!payload.d));
                 break;
             }
             case DiscordTypes.GatewayOpcodes.Hello: {
