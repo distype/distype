0834db2e678b9f955617cc8988b1bd4dae6c24e8
Bryce Roethel <br88c@br88c.dev>
2022-07-30 22:02:53 -0400

    Bump

--- src/constants/DistypeConstants.ts
+++ src/constants/DistypeConstants.ts
@@ -5,7 +5,7 @@ export const DistypeConstants = {
     /**
      * The package's version.
      */
-    VERSION: `0.19.0`,
+    VERSION: `1.0.0`,
     /**
      * The URL to Distype's source.
      */
