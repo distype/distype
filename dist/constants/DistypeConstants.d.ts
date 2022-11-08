e8dd5d8aa860ab8a5d3957521ccce5b70fb6ad78
BR88C <br88c@br88c.dev>
2022-11-08 18:34:22 +0000

    build: TSC build for 6875cdc4b4589e9182426fd633c20e8c59ad7844

--- dist/constants/DistypeConstants.d.ts
+++ dist/constants/DistypeConstants.d.ts
@@ -5,7 +5,7 @@ export declare const DistypeConstants: {
     /**
      * The package's version.
      */
-    readonly VERSION: "1.2.0";
+    readonly VERSION: "1.2.1";
     /**
      * The URL to Distype's source.
      */
--- dist/constants/DistypeConstants.js
+++ dist/constants/DistypeConstants.js
@@ -8,7 +8,7 @@ exports.DistypeConstants = {
     /**
      * The package's version.
      */
-    VERSION: `1.2.0`,
+    VERSION: `1.2.1`,
     /**
      * The URL to Distype's source.
      */
