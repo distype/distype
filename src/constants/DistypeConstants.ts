6875cdc4b4589e9182426fd633c20e8c59ad7844
BR88C <br88c@br88c.dev>
2022-11-08 13:33:48 -0500

    Bump

--- src/constants/DistypeConstants.ts
+++ src/constants/DistypeConstants.ts
@@ -5,7 +5,7 @@ export const DistypeConstants = {
     /**
      * The package's version.
      */
-    VERSION: `1.2.0`,
+    VERSION: `1.2.1`,
     /**
      * The URL to Distype's source.
      */
