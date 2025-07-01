f66d9d9f04f6a86045077633259b12cc75e2d6fe
bryceroethel <me@bryceroethel.com>
2025-07-01 00:40:12 +0000

    build: TSC build for 71316880224357e12887dbd09add178b880e9b36

--- dist/constants/DistypeConstants.d.ts
+++ dist/constants/DistypeConstants.d.ts
@@ -5,7 +5,7 @@ export declare const DistypeConstants: {
     /**
      * The package's version.
      */
-    readonly VERSION: "1.2.1";
+    readonly VERSION: "3.0.1";
     /**
      * The URL to Distype's source.
      */
